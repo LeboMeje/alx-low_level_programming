5bb27e88a768a66d64e2f1e2ceae102429a95019
LeboMeje <lebomhopewell@gmail.com>
2022-09-08 16:49:47 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#!/bin/bash
 #include<stdio.h>
 
+
+
 /**
+ *main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 
