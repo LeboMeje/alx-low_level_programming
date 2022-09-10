f68b2b85b646ecd86baadaa09ec1a09f933359e9
LeboMeje <lebomhopewell@gmail.com>
2022-09-10 11:52:30 -0700

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,15 @@
 int main(void)                                                                                  
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxy";
-	int i;		                                                                    		for (i = 0; i < 26; i++)                                                                
+	
+	int i;
+
+	for (i = 0; i < 26; i++)                                                                
 {
-		putchar(alp[i]);        
+	putchar(alp[i]);        
 }
+{
 	putchar('\n');
+}
 	return (0);                                                                             
 }
