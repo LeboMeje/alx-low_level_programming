f0090adbb1ae408cdb36f538c7971ab7d18e77a4
LeboMeje <lebomhopewell@gmail.com>
2022-09-12 04:42:11 -0700

    Print comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,46 +9,34 @@ int main(void)
 
 {
 	int i, e, g, h, op1, op2;
-
 	i = e = g = h = 48;
-
 	while (h < 58)
 	{
 		g = 48;
-
 		while (g < 58)
 		{
 			e = 48;
-
 			while (e < 58)
 			{
 				i = 48;
-
 				while (i < 58)
 				{
 					op1 = (h * 10) + g;
-
 					op2 = (e * 10) + i;
-
 					if (op1 < op2)
 					{
 						putchar(h);
-
 						putchar(g);
-
 						putchar(' ');
-
 						putchar(e);
-
 						putchar(i);
-
+			
 						if (h == 57 && g == 56 && e == 57 && i == 57)
 
-							break;
+						break;
 
-						putchar(',');
-
-						putchar(' ');
+					putchar(',');
+					putchar(' ');
 					}
 					i++;
 				}
@@ -59,7 +47,6 @@ int main(void)
 		h++;
 	}
 	putchar('\n');
-
 	return (0);
 }
 
