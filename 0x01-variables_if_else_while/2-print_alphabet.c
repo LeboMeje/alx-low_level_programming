234e38160bd2f328eb5ddb49093c29c11c1d9747
LeboMeje <lebomhopewell@gmail.com>
2022-09-11 23:50:23 -0700

    Print alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,16 @@
  * main - Prints the alphabet
  * Return: Always 0 (Success)
  *
- */
+*/
+
 int main(void)
 
 {
+
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
 	int i;
+
 	for (i = 0; i < 26; i++)
 {
 	putchar(alp[i]);
