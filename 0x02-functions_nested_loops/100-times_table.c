fd8165d1d3467449e9e137694ed30d80ba2918f0
LeboMeje <lebomhopewell@gmail.com>
2022-09-14 15:48:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,16 +15,6 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-
-    
-          
-            
-    
-
-          
-    
-    
-  
 			_putchar(48);
 			for (b = 1; b <= n; b++)
 			{
