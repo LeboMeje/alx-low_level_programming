288226ddf168529a9c13290eaa28e2be931a6189
LeboMeje <lebomhopewell@gmail.com>
2022-09-14 08:34:20 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar(รร('u');
+	_putchar('u');
 	_putchar('t');
-	_putchar(ร'c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n')
 	
 	return (0);
 
