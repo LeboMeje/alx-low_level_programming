e056af31737bcd5bbc941f0aa3321e5e58efa28e
LeboMeje <lebomhopewell@gmail.com>
2022-09-14 06:26:11 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- *
+ * main - print putchar
  * Return: always 0
  */
 
 int main(void)
 {
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar(รร('u');
+	_putchar('t');
+	_putchar(ร'c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	
 	return (0);
+
 }
