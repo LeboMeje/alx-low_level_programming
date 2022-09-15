653b55e3f131be4bb3adedbfb1f27415892cc468
LeboMeje <lebomhopewell@gmail.com>
2022-09-15 03:20:31 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,6 +5,7 @@
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  */
+
 int _isupper(int c)
 
 {
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,4 +14,3 @@ void print_triangle(int size);
 void print_number(int n);
 
 #endif /* MAIN_H */
-
