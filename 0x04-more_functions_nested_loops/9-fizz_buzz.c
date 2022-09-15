4b53e3940df8cb90d9b1cb19d9e994e68545c8de
LeboMeje <lebomhopewell@gmail.com>
2022-09-15 11:56:19 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,13 +4,14 @@
 /**
  *main - prints Buzz each numbers of 3 and 5.
  *Return: Always 0.
- */
+*/
+
 int main(void)
 {	
 	int n;
 
 	n = 1;
-	printf(" %d", n);
+	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
