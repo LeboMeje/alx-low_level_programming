6f66fde6fd27284dbcd69b3ccae4fb5053d075e3
LeboMeje <lebomhopewell@gmail.com>
2022-09-15 09:32:14 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - prints Buzz each numbers of 3 and 5.
- * Return: Always 0.
+ *main - prints Buzz each numbers of 3 and 5.
+ *Return: Always 0.
  */
 int main(void)
 {	
