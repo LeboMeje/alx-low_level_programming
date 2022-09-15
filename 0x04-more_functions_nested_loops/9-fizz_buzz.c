5115824e13964292e1474dc63aef4db964b6433c
LeboMeje <lebomhopewell@gmail.com>
2022-09-15 11:51:04 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,24 @@ int main(void)
 	int n;
 
 	n = 1;
-	printf("%d", n);
+	printf(" %d", n);
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-		printf("FizzBuzz");
+		printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-		printf("Fizz");
+		printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-		printf("Buzz");
+		printf(" Buzz");
 		}
 		else
 		{
-		printf("%d", n);
+		printf(" %d", n);
 		}
 	}
 	printf("\n");
