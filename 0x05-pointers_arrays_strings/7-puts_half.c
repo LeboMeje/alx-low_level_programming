704620f6a62848ff8b4cc975e21a517c7da9058f
LeboMeje <lebomhopewell@gmail.com>
2022-09-19 20:57:41 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,21 @@
  * @str: input string.
  * Return: no return.
  */
-
 void puts_half(char *str)
 {
 	int count = 0, i;
 
 	while (count >= 0)
 	{
-	if (str[count] == '\0')
-		break;
+		if (str[count] == '\0')
+			break;
 		count++;
 	}
+
 	if (count % 2 == 1)
-	i = count / 2;
+		i = count / 2;
 	else
-	i = (count - 1) / 2;
+		i = (count - 1) / 2;
 	for (i++; i < count; i++)
 		putchar(str[i]);
 	putchar('\n');
