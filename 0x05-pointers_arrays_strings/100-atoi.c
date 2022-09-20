b73441d526ad9afb848922d0c65571d3a2c5dd9d
LeboMeje <lebomhopewell@gmail.com>
2022-09-20 01:27:23 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,13 +14,15 @@ int _atoi(char *s)
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
+
 		if (*(s + count) == '-')
 			pn *= -1;
+
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
-			size++
+			size++;
 		}
 		count++;
 	}
