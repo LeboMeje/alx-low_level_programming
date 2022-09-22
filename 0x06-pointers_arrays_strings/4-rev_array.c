41fdf39a1632e4b1e60c4cc781931e9347425924
LeboMeje <lebomhopewell@gmail.com>
2022-09-22 08:50:36 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,7 +17,8 @@ void reverse_array(int *a, int n)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
-			temp = *(a + j);															*(a + j) = *(a + (j - 1));
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
 			*(a + (j - 1)) = temp;
 		}
 	}
