a195510b8058265095f63bfe57e74630b6517b28
LeboMeje <lebomhopewell@gmail.com>
2022-09-26 14:23:43 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,7 +13,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
+		putchar(45);
 		m = n * -1;
 	}
 	else
@@ -32,6 +32,6 @@ void print_number(int n)
 
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+		putchar(((m / count) % 10) + 48);
 	}
 }
