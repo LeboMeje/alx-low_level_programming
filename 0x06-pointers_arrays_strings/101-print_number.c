1ac85bd3bcad15119391a77f14d864024e72c65e
LeboMeje <lebomhopewell@gmail.com>
2022-09-27 02:06:26 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,13 @@
  * @n: input integer.
  * Return: no return.
  */
-
 void print_number(int n)
 {
 	unsigned int m, d, count;
-
+	int _putchar(char);
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		m = n * -1;
 	}
 	else
@@ -32,6 +31,6 @@ void print_number(int n)
 
 	for (; count >= 1; count /= 10)
 	{
-		putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
