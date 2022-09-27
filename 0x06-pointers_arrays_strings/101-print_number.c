9695d5d9f0659e8443523527ba5de95a16c80765
LeboMeje <lebomhopewell@gmail.com>
2022-09-27 02:38:27 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - prints an integer.
+ * @n: input integer.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_number(98);
+
+	_putchar('\n');
+
+	print_number(402);
+
+	_putchar('\n');
+
+	print_number(1024);
+
+	_putchar('\n');
+
+	print_number(0);
+
+	_putchar('\n');
+
+	print_number(-98);
+
+	_putchar('\n');
+
+	return (0);
+}
