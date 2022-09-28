7228d71df436d2fb14d02f4a6c6cc343cde688d6
LeboMeje <lebomhopewell@gmail.com>
2022-09-28 05:26:47 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * wildcmp - compares two strings.
  * @s1: string 1.
- * @@ -8,15 +8,13 @@
- * Return: Always 0.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
  */
 
 int wildcmp(char *s1, char *s2)
