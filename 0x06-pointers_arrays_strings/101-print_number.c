b5d10f2b2405b1704bdbfbab3d6f5a85701a827c
LeboMeje <lebomhopewell@gmail.com>
2022-09-28 09:55:13 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,35 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - prints an integer.
+ * print_number - prints an integer.
  * @n: input integer.
- * Return: Always 0.
+ * Return: no return.
  */
-
-int main(void)
+void print_number(int n)
 {
-	print_number(98);
+	unsigned int m, d, count;
 
-	_putchar('\n');
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-	print_number(402);
+	d = m;
+	count = 1;
 
-	_putchar('\n');
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
 
-	print_number(1024);
-
-	_putchar('\n');
-
-	print_number(0);
-
-	_putchar('\n');
-
-	print_number(-98);
-
-	_putchar('\n');
-
-	return (0);
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
