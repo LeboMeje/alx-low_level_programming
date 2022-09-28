c2caded21c38825319a53e09d37589146b94c108
LeboMeje <lebomhopewell@gmail.com>
2022-09-28 10:37:11 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
+int _putchar(char);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
