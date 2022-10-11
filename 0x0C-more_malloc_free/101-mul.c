fc10a01d483ad91f7d05905c5d1757a0b6166fc5
LeboMeje <lebomhopewell@gmail.com>
2022-10-11 13:42:06 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,24 +14,23 @@ void _is_zero(char *argv[])
 	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
+	if (argv[1][i] != '0')
+	{
+		isn1 = 0;
+		break;
+	}
 	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
+	if (argv[2][i] != '0')
+	{
+		isn2 = 0;
+		break;
+	}
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
 	}
 }
-
 /**
  * _initialize_array - set memery to zero in a new array
  * @ar: char array.
@@ -48,7 +47,6 @@ char *_initialize_array(char *ar, int lar)
 	ar[lar] = '\0';
 	return (ar);
 }
-
 /**
  * _checknum - determines length of the number
  * and checks if number is in base 10.
@@ -62,14 +60,13 @@ int _checknum(char *argv[], int n)
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
+	if (!isdigit(argv[n][ln]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	return (ln);
 }
-
 /**
  * main - Entry point.
  * program that multiplies two positive numbers.
@@ -121,4 +118,3 @@ int main(int argc, char *argv[])
 	printf("%s\n", nout);
 	return (0);
 }
-
