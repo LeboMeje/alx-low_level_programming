81a6419e00d0dd3e70355f5f33a17cc196f4e173
LeboMeje <lebomhopewell@gmail.com>
2022-10-31 05:33:14 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,9 +8,11 @@
  * @n: value of the element
  * Return: the address of the new element
  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *h;
+
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
