7da533216acdf39f1b7d9a7fd93f80a4f8fe01b4
LeboMeje <lebomhopewell@gmail.com>
2023-01-08 02:54:45 -0800

    adding a function that generates a distributed index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,23 +1,17 @@
 #include "hash_tables.h"
+
 /**
- * key_index - function that generates a distributed index
- * according to a given hash
+ * key_index - Get the index at which a key/value pair should
+ *             be stored in array of a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
  *
- * @key: key passed
- * @size: size of the hash tables
- * Return: key index
-#include "hash_tables.h"
-/**
- * key_index - function that generates a distributed index
- * according to a given hash
+ * Return: The index of the key.
  *
- * @key: key passed
- * @size: size of the hash tables
- * Return: key index
+ * Description: Uses the djb2 algorithm.
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == NULL || size == 0)
-		return (0);
 	return (hash_djb2(key) % size);
 }
